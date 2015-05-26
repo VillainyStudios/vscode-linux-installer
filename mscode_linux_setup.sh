243e8b31376cc29037eee97779197fc241cdc0ec
Joshua Embrey <mrschism@sdf.org>
2015-05-26 16:47:43 -0400

    Added base functionality
    It should work.

--- mscode_linux_setup.sh
+++ mscode_linux_setup.sh
@@ -34,31 +34,56 @@ function prestart {
 # Fetch archive
 function fetch_archive {
   if [[ $os = "Linux" ]]; then
-    wget -t0 http://go.microsoft.com/fwlink/?LinkID=534108
+    wget -t0 -O ~/vscode-linux.zip http://go.microsoft.com/fwlink/?LinkID=534108
 ### OSX Install Fetch ###
 # else
-#   wget -t0 http://go.microsoft.com/fwlink/?LinkID=534108
+#   wget -t0 -O ~/vscode-osx.zip http://go.microsoft.com/fwlink/?LinkID=534108
   fi
+}
+
+# Extract Archive
+function extract_archive {
+  # Extract for linux
+  unzip -LL -q ./VSCode-linux.zip -d ./code/
+  # Extract for OSX
+  # CODE GOES HERE
+}
+
+# Extract code to FHS suggested location for "install"
+function install_code {
+  unzip -LL -q /vscode-linux.zip -d /opt/vscode/
 }
 
 # Create .desktop file
 function make_desktop_file {
-  echo '[Desktop Entry]' > ./code.desktop
-  echo 'Type=Application' >> ./code.desktop
-  echo 'Version=0.1.0' >> ./code.desktop
-  echo 'Name=Code' >> ./code.desktop
-  echo 'Comment=Visual Studio Code Editor' >> ./code.desktop
-  echo 'Exec=code' >> ./code.desktop
-  echo 'Icon=vso' >> ./code.desktop
-  echo 'Terminal=false' >> ./code.desktop
-  echo 'Categories=Programming;Development;' >> ./code.desktop
+  echo '[Desktop Entry]' > ~/code.desktop
+  echo 'Type=Application' >> ~/code.desktop
+  echo 'Version=0.1.0' >> ~/code.desktop
+  echo 'Name=Code' >> ~/code.desktop
+  echo 'Comment=Visual Studio Code Editor' >> ~/code.desktop
+  echo 'Exec=code' >> ~/code.desktop
+  echo 'Icon=vso' >> ~/code.desktop
+  echo 'Terminal=false' >> ~/code.desktop
+  echo 'Categories=Programming;Development;' >> ~/code.desktop
 }
 
 # Put files into appropriate folders
 function move_files {
-  mv ./code.desktop /usr/share/applications
+  mv ~/code.desktop /usr/share/applications
   cp ./resources/app/vso.png /usr/share/icons/vso.png
 }
+
+### Main Call ### 
+echo "Please select an option"
+select ans in 'Fetch & Install' 'Fetch & Extract ONLY' "Fetch ONLY" "Install ONLY" "EXIT"; do
+  case $ans in
+    1 ) fetch_archive; install_code; create_desktop-file; move_files;
+    2 ) fetch_archive; extract_archive;
+    3 ) fetch_archive;
+	  4 ) echo "COMING SOON"; 
+    5 ) echo "";
+    esac
+done
 
 # Exit cleanly
 exit 0
